af3dfb12ee675f04c0652c35793f53ab4336dfc9
Mirko <mirkoalicastro@gmail.com>
2018-10-23 17:30:02 +0200

    Added contact listener

---
+++ app/src/main/java/com/mirkoalicastro/angrywhat/physics/MyContactListener.java
@@ -0,0 +1,14 @@
+package com.mirkoalicastro.angrywhat.physics;
+
+import com.google.fpl.liquidfun.Contact;
+import com.google.fpl.liquidfun.ContactListener;
+
+public class MyContactListener extends ContactListener {
+    public MyContactListener() {
+        super();
+    }
+    public void beginContact(Contact contact) {
+        super.beginContact(contact);
+
+    }
+}
