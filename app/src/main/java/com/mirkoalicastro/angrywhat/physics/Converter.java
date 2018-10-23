5da9c0ab114d725085ead10a9d53a37e226c9620
Mirko <mirkoalicastro@gmail.com>
2018-10-23 17:31:48 +0200

    Moved Converter class

---
+++ app/src/main/java/com/mirkoalicastro/angrywhat/physics/Converter.java
@@ -0,0 +1,16 @@
+package com.mirkoalicastro.angrywhat.physics;
+
+public class Converter {
+    private static final float PHYSIC_HEIGHT = 15;
+
+    private static float SCALE;
+
+    public static void setScale(int frameWidth, int frameHeight){
+        SCALE = (float)frameHeight/PHYSIC_HEIGHT;
+    }
+
+    public static float physicsToFrame(float val){ return val*SCALE; }
+
+    public static float frameToPhysics(float val){ return val/SCALE; }
+
+}
